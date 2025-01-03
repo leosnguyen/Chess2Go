11cc43daf47a7cdb4ed74db1c8bf043ce40e2286
leosnguyen <leosnguyen07@gmail.com>
2025-01-03 20:02:26 +0100

    004_COMMIT

--- app/src/main/java/cz/chess2go/OpeningsActivity.java
+++ app/src/main/java/cz/chess2go/OpeningsActivity.java
@@ -1,6 +1,8 @@
 package cz.chess2go;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class OpeningsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void backToMenu(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
