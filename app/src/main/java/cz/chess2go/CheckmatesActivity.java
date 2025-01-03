0d8a8aee177ab99184ceb4614b417453213393bb
leosnguyen <leosnguyen07@gmail.com>
2025-01-03 16:58:16 +0100

    001_COMMIT

--- app/src/main/java/cz/chess2go/CheckmatesActivity.java
+++ app/src/main/java/cz/chess2go/CheckmatesActivity.java
@@ -1,8 +1,6 @@
 package cz.chess2go;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,33 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CheckmatesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_checkmates);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void openChess(View view) {
-        startActivity(new Intent(this, ChessActivity.class));
-    }
-
-    public void openOpenings(View view) {
-        startActivity(new Intent(this, OpeningsActivity.class));
-    }
-
-    public void openCheckmates(View view) {
-        startActivity(new Intent(this, CheckmatesActivity.class));
-    }
-
-    public void openTactics(View view) {
-        startActivity(new Intent(this, TacticsActivity.class));
     }
 }
